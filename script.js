6d3f52158262bf70c3829d56744cf5e80c07dd79
tanvibelsare <118069064+tanvibelsare@users.noreply.github.com>
2023-04-23 11:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let username=document.getElementById("username");
+let password=document.getElementById("password");
+let checkbox=document.getElementById("checkbox");
+let btn=document.getElementById("submit");
+let exist=document.getElementById("existing");
+btn.addEventListner("click",(event)=>{
+	
+	event.preventDefault()
+	if(checkbox.checked =="true"){
+		localStorage.setItem("Username",username.value)
+		localStorage.setItem("Password",password.value)
+		alert("Logged in as"+username.value)
+	}else{
+		localStorage.removeItem("Username",username.value)
+		localStorage.removeItem("Passsword",password.value)
+	}
+})
+exist.addEventListener("click",()=>{
+	// if(username.value!=none||password.value!=nne){o
+		alert("Logged in as"+username.value)
+	// }
+})
