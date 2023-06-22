a91151f70faa63812e613f81df6fa467acf79c57
tanvibelsare <118069064+tanvibelsare@users.noreply.github.com>
2023-06-22 17:43:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,17 +7,28 @@ let exist=document.getElementById("existing");
 btn.addEventListner("click",(event)=>{
 	
 	event.preventDefault()
-	if(checkbox.checked =="true"){
+	console.log("hii")
+	if(checkbox.checked){
 		localStorage.setItem("Username",username.value)
 		localStorage.setItem("Password",password.value)
-		alert("Logged in as"+username.value)
+		
 	}else{
 		localStorage.removeItem("Username",username.value)
 		localStorage.removeItem("Passsword",password.value)
 	}
+	alert("Logged in as"+username.value)
 })
-exist.addEventListener("click",()=>{
-	// if(username.value!=none||password.value!=nne){o
-		alert("Logged in as"+username.value)
-	// }
-})
+ if (localStorage.getItem("Username")) {
+      var existingUserBtn = document.createElement("button");
+      existingUserBtn.id = "existing";
+      existingUserBtn.textContent = "Login as existing user";
+      document.body.appendChild(existingUserBtn);
+      existingUserBtn.addEventListener("click", function() {
+        var savedUsername = localStorage.getItem("username");
+        alert("Logged in as " + savedUsername);
+      });
+    }
+// exist.addEventListener("click",()=>{
+// 	// if(username.value!=none||password.value!=nne){o
+// 		alert("Logged in as"+username.value)
+// 	// }
